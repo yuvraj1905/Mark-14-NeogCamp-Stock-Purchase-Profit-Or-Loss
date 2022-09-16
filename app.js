7924fbe9112caac75d2447c42fdf75f01fc61882
yuvraj1905 <yuvrajkumar2509@gmail.com>
2022-09-16 20:33:53 +0530

    fix: code logic

--- app.js
+++ app.js
@@ -6,41 +6,48 @@ const submitbtn = document.querySelector("#submit-btn");
 const Output = document.querySelector("#Output");
 
 function checkPrice() {
-  var startPrice = Number(initialPrice.value);
-  var quantity = Number(numberOfStocks.value);
-  var currentValue = Number(currentPrice.value);
+  var startPrice = initialPrice.value;
+  var quantity = numberOfStocks.value;
+  var currentValue = currentPrice.value;
 
   if (startPrice === "" || quantity === "" || currentValue === "") {
     alert("Please enter all required values ");
+    return;
   }
 
-  var moneyInvested = startPrice * quantity;
-  var currentEstimate = currentValue * quantity;
+  var moneyInvested = Number(startPrice) * Number(quantity);
+  var currentEstimate = Number(currentValue) * Number(quantity);
 
   var result = checkProfitOrLoss(moneyInvested, currentEstimate);
   // console.log(result);
 }
 
 function checkProfitOrLoss(initial, now) {
+  var loss = initial - now;
+  var profit = now - initial;
+  var lossPercent = (((initial - now) * 100) / initial).toFixed(2);
+  var profitPercent = (((now - initial) * 100) / initial).toFixed(2);
   var result =
     initial > now
-      ? `Loss of ${initial - now} and `
-      : `Profit of ${now - initial} and `;
+      ? `Feels bad!! You got a loss of Rs ${loss} which is `
+      : `Woahhh! You got a profit of Rs ${profit} which is `;
   var resultPercent =
     initial > now
-      ? `you got a loss of ${(((initial - now) * 100) / initial).toFixed(2)}%.`
-      : `you got a Profit of ${(((now - initial) * 100) / initial).toFixed(
-          2
-        )}%.`;
+      ? `${lossPercent}% of loss over initial value😐.`
+      : ` ${profitPercent}% of profit over the initial value 🤩💰.`;
 
   var ans;
   if (initial === now) {
-    ans = "No gain No Loss";
+    ans = "No gain No Loss 🤠";
   } else {
     ans = result + resultPercent;
     Output.innerText = ans;
   }
   Output.innerText = ans;
+  Output.style.display = "block";
+  if (lossPercent > 50) {
+    document.body.style.backgroundColor = "red";
+  }
 }
 
 submitbtn.addEventListener("click", checkPrice);
